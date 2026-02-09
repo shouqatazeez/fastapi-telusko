822bc335343d2528414ab9fb176fa65c6ede34bd
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-02-09 20:48:39 +0530

    work with base model

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
 
+ 
+
+from pydantic import BaseModel
+
+class product(BaseModel):
+    id:int
+    name:str
+    description:str
+    price:float
+    quantity:int
+
+
+
+
 app = FastAPI()
 
+
+@app.get("/")
 def intro():
     return "This is the intrdouction to fastapi in simple" 
 
+
+products = [
+    product(id = 1,name = "Mobile",description = "This is the smart mobile",price = "4.5", quantity =10)
+]
+
+@app.get("/products")
+def productslist():
+    return products
---
+++ models.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class product(BaseModel):
+    id:int
+    name:str
+    description:str
+    price:float
+    quantity:int
