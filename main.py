fc9410182b0c515920208bcebc5acf153a69a345
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-02-10 21:16:49 +0530

    practiced about the how to fetch the products with their id

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 from fastapi import FastAPI
-
- 
-
-from pydantic import BaseModel
-
-class product(BaseModel):
-    id:int
-    name:str
-    description:str
-    price:float
-    quantity:int
-
-
-
+from models import product
 
 app = FastAPI()
 
 
 @app.get("/")
 def intro():
-    return "This is the intrdouction to fastapi in simple" 
+    return "This is the introduction to fastapi in simple" 
 
 
 products = [
-    product(id = 1,name = "Mobile",description = "This is the smart mobile",price = "4.5", quantity =10)
+    product(id = 1,name = "Mobile",description = "This is the smart mobile",price = "45", quantity =10),
+    product(id=2,name = "Laptop", description="This is how we use for personal purpose", price = "20",quantity=20),
+    product(id=3,name="TV", description= "This is how we watch for the purpose of Entertainment", price="30", quantity= 25)
 ]
 
 @app.get("/products")
 def productslist():
     return products
+
+@app.get("/products/{id}")
+def productid(id:int):
+    return products[id]
+    
