6f3643050e1cb7c6d4913aecd3ac88c435454b23
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-02-14 19:02:50 +0530

    add how to link with the database details

--- database.py
+++ database.py
@@ -4,4 +4,4 @@ from sqlalchemy import create_engine
 
 db_url = "postgresql://postgres:shouqat_7@localhost:5432/fastapi_db"
 engine =  create_engine(db_url)
-Session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
